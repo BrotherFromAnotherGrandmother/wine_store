f1a50cb7ab196580a13812608eb010b77a282473
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-26 16:08:51 +0300

    Шаг 8 Создал генерацию файла index.html

--- wine-master/main.py
+++ wine-master/main.py
@@ -1,5 +1,26 @@
+from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-print(11234532234)
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+based = 1920
+now = datetime.now().year
+delta = now - based
+
+
+rendered_page = template.render(
+    years_counter=delta,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
 server = HTTPServer(('0.0.0.0', 8002), SimpleHTTPRequestHandler)
 server.serve_forever()
